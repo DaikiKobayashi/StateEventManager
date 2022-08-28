721e7b80fc69c6112362d83c0e2eeecfae5605f9
DaikiKobayashi <55055934+DaikiKobayashi@users.noreply.github.com>
2022-08-29 00:40:25 +0900

    Add files via upload

---
+++ AnimationNotify/StateChangeEventReceiver.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UniRx;
+
+namespace StateEventManager
+{
+    public class StateChangeEventReceiver : MonoBehaviour
+    {
+        private Animator m_animator;
+        private StateChangeObserver m_observer;
+        private EventListener m_currentListener;
+
+        private void Start()
+        {
+            Debug.AssertFormat(TryGetComponent(out m_animator), "please component. : Animator", this);
+
+            m_observer = m_animator.GetBehaviour<StateChangeObserver>();
+            Debug.AssertFormat(m_observer != null, "please StateMachineBehaviour. : StateChangeObserver", m_animator);
+
+            m_observer?
+                .StateChangeObservable
+                .Subscribe(OnStateChange)
+                .AddTo(this);
+        }
+
+        private void OnStateChange(Animator animator)
+        {
+            m_currentListener?
+                .ListenerEvent
+                .Invoke(animator);
+        }
+
+        private void ChangeEventListener(EventListener listener)
+        {
+            m_currentListener = listener;
+        }
+    }
+
+    public class EventListener
+    {
+        public EventListener(UnityAction<Animator> listernerEvent)
+        {
+            ListenerEvent = listernerEvent;
+        }
+
+        public UnityAction<Animator> ListenerEvent { get; private set; }
+    }
+}
